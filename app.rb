58f4f5c09a4b4fdf5eabdd81967fbe620bea0387
Audrey Tang <audreyt@audreyt.org>
2014-03-10 02:39:27 +0800

    * Try fixing ?:format? (ea5f1697) breaking single-line quotation. Reported by @ipaaa

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ module IRC_Log
       redirect "/channel/#{channel}/today"
     end
 
-    get "/channel/:channel/:date/?:format?" do |channel, date, format|
+    get %r{/channel/([-.\w]+)/([-\d]+)/?(json)?} do |channel, date, format|
       case date
         when "today"
           @date = Time.now.strftime("%F")
